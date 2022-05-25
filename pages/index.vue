0dd8d899692c080ec64ff98ae26de29825d7f7e0
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2022-05-25 18:31:41 +0800

    start

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <Hero-file />
+</template>
+
+<script>
+import HeroFile from '~/components/Hero-file.vue'
+export default {
+  components: { HeroFile },
+  name: 'IndexPage'
+}
+</script>
